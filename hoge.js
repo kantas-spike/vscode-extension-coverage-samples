ba6b635d21a4cbd69936b95a370d0a663a92832c
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-10-03 01:36:45 +0900

    env: カバレッジ結果の確認用に分岐有コードを追加

---
+++ hoge.js
@@ -0,0 +1,11 @@
+function isMinus(num) {
+    if (num >= 0) {
+        return false
+    } else {
+        return true
+    }
+}
+
+module.exports = {
+    isMinus
+}
--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -3,7 +3,8 @@ const assert = require('assert');
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 const vscode = require('vscode');
-// const myExtension = require('../extension');
+const myExtension = require('../../extension');
+const hoge = require('../../hoge')
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
@@ -11,5 +12,13 @@ suite('Extension Test Suite', () => {
 	test('Sample test', () => {
 		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
 		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+		assert.ok(myExtension.activate)
 	});
+
+	test("hoge", () => {
+		assert.strictEqual(false, hoge.isMinus(0))
+		assert.strictEqual(false, hoge.isMinus(100))
+		assert.strictEqual(true, hoge.isMinus(-1))
+		assert.strictEqual(true, hoge.isMinus(-100))
+	})
 });
