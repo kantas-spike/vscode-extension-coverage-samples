cdb7b4735c74891fc24f4226d82ae375e8f7487f
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-10-03 05:31:54 +0900

    ダメだけど一旦コミット

--- test/suite/index.js
+++ test/suite/index.js
@@ -1,6 +1,40 @@
 const path = require('path');
 const Mocha = require('mocha');
 const glob = require('glob');
+const { createInstrumenter } = require('istanbul-lib-instrument');
+const instrumenter = createInstrumenter();
+const { hookRequire } = require('istanbul-lib-hook');
+const TestExclude = require('test-exclude')
+var libCoverage = require('istanbul-lib-coverage');
+
+function setupCoverage(testsRoot) {
+	const projectRoot = path.resolve(path.join(testsRoot, ".."))
+	console.log("project: ", projectRoot)
+	const matcher = new TestExclude({ cwd: projectRoot,  exclude: [".vscode/extensions/**", "node_modules/**", "test/**", "coverage/**"], extension: ['.js'] })
+	hookRequire((filePath) => {
+		const r = matcher.shouldInstrument(filePath)
+		// console.log(filePath, r)
+		return r
+	}, (code, { filename }) => instrumenter.instrumentSync(code, filename));
+}
+
+function reportCoverage(testsRoot) {
+  const projectRoot = path.resolve(path.join(testsRoot, ".."))
+  console.log("dir:", projectRoot)
+  var coverageMap = libCoverage.createCoverageMap(global.__coverage__);
+
+  const libReport = require("istanbul-lib-report");
+  const reports = require("istanbul-reports");
+
+    // create a context for report generation
+  const context = libReport.createContext({ dir: path.join(projectRoot, "coverage"), coverageMap });
+
+    // create an instance of the relevant report class, passing the
+  // report name e.g. json/html/html-spa/text
+  const report = reports.create("html");
+// call execute to synchronously create and write the report to disk
+  report.execute(context);
+}
 
 function run() {
 	// Create the mocha test
@@ -10,6 +44,7 @@ function run() {
 	});
 
 	const testsRoot = path.resolve(__dirname, '..');
+	setupCoverage(testsRoot)
 
 	return new Promise((c, e) => {
 		const testFiles = new glob.Glob('**/**.test.js', { cwd: testsRoot });
@@ -32,6 +67,8 @@ function run() {
 						c();
 					}
 				});
+				console.log("END!!")
+				reportCoverage(testsRoot)
 			} catch (err) {
 				console.error(err);
 				e(err);
