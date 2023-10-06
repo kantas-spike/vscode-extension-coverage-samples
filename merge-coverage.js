6f20cc7f720f3bc18df4d4a52829385e9667471b
kanta <101532773+kantas-spike@users.noreply.github.com>
2023-10-07 06:40:52 +0900

    パッケージ化に伴い不要ファイルを削除

--- merge-coverage.js
+++
@@ -1,6 +0,0 @@
-const path = require("path");
-const covUtils = require("./lib/cov-utils")
-
-const projectRoot = path.resolve(".")
-const config = covUtils.readConfig(projectRoot)
-covUtils.mergeCoverageFromRaw("merged", config)
